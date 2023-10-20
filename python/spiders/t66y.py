e2a3db62cb091cd9de2b5bdb39a31b4cc3a54c61
WeiCaoMelbourne <weicao.melbourne@gmail.com>
2023-10-21 02:36:59 +1100

    pretty much done

--- python/spiders/t66y.py
+++ python/spiders/t66y.py
@@ -1,103 +1,103 @@
 import requests
-# from lxml import etree
 from bs4 import BeautifulSoup
-import re
+import os
 
-url = "https://t66y.com/htm_data/1404/20/999224.html"
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'
 }
 
-def txt(page_url=None):
-    if not page_url:
-        page_url = url
+def main():
+    url = "https://t66y.com/htm_data/1404/20/999224.html"
+    domain = "https://t66y.com/"
+    title = None
+    author = None
 
-    DIV_CLEANR = re.compile('<div.*?/div>') 
-    FONT_CLEANR = re.compile('<font.*?/font>') 
-
-    try:
-        response = requests.get(page_url, headers=headers)
-        if response.status_code == 200:
-
-            page_content = response.text
-            response.close()
-
-            # print(page_content)
-            page = BeautifulSoup(page_content, "html.parser")
-            divs = page.find_all("div", attrs={
-                "class": "t t2"
+    def extract_save(div):
+        print("extract_save")
+        content_el = div.find("div", attrs={
+            "class": "tpc_content"
             })
 
-            i = 0
-            for div in divs:
-                tr = div.find("tr", attrs={
-                    "class": "tr1 do_not_catch"
-                    })
+        # to remove all tags in this content
+        for sub_div in content_el.find_all("div"):
+            sub_div.decompose()
+
+        for sub_div in content_el.find_all("font", attrs={"color":"gray"}):
+            sub_div.decompose()
+
+        content = content_el.text.strip()
+        content = content.replace("　　", "\r\n")
+
+        with open(f"{title}.txt", mode="a", encoding="utf-8") as file:
+            # Write the string to the file
+            file.write("\r\n" + content)
+
+    
+    def page_download(page_url):
+        print(page_url)
+        nonlocal title, author
+        try:
+            response = requests.get(page_url, headers=headers)
+            if response.status_code == 200:
+
+                page_content = response.text
+                response.close()
+
+                # print(page_content)
+                page = BeautifulSoup(page_content, "html.parser")
                 
-                if tr:
-                    author_el = tr.find("b")
-                    if not author_el:
-                        continue
-                    # print(author_el.text.strip().split()[0])
-                    if i == 0:
-                        # 作者
-                        author = author_el.text.strip().split()[0]
-                        content_el = div.find("div", attrs={
-                            "class": "tpc_content do_not_catch"
-                            })
-                        # print("<br>" in content_el.extract())
-
-                        # extract()可以保留<br>, text去掉所有tag
-                        # print(content_el.extract())
-                        # content_el.div.decompose()
-                        # content_el.font.decompose()
-                        for sub_div in content_el.find_all("div"):
-                            sub_div.decompose()
-
-                        for sub_div in content_el.find_all("font", attrs={"color":"gray"}):
-                            sub_div.decompose()
-
-                        content = content_el.text.strip()
-                        
-                        content = content.replace("　　", "\r\n")
-
-                        with open("1.txt", mode="w", encoding="utf-8") as file:
-                            # Write the string to the file
-                            file.write("\r\n" + content)
-
-                        # content = content_el.extract()
-                        # print(content)
-                        # cleantext = re.sub(DIV_CLEANR, '', str(content))
-                        # cleantext = re.sub(FONT_CLEANR, '', str(cleantext))
-                        # cleantext = cleantext.replace('<br/><br/>', "\r\n")
-                        # cleantext = cleantext.replace('<br/>', "")
-                        # # print(cleantext)
-                        # with open("1.txt", mode="w", encoding="utf-8") as file:
-                        #     # Write the string to the file
-                        #     file.write(cleantext)
-                    else:
-                        # 只看作者
+                if not title:
+                    title_el = page.find("title").text
+                    title = title_el.split('-')[0].strip()
+                    try:
+                        os.remove(f"{title}.txt")
+                    except OSError:
                         pass
-                        # if author in author_el.text.strip().split()[0]:
-                        #     content_el = div.find("div", attrs={
-                        #         "class": "tpc_content do_not_catch"
-                        #         })
-                        #     print(content_el.text.strip())
+                    title = 1   # TODO 
 
-                i += 1
-                # content_el = div.find("div", attrs={
-                #     "class": "c"
-                #     })
-                # if not content_el:
-                #     continue
+                divs = page.find_all("div", attrs={
+                    "class": "t t2"
+                })
+                for div in divs:
+                    tr = div.find("tr", attrs={
+                        "class": "tr1"
+                        })
+                    
+                    if tr:
+                        author_el = tr.find("b")
+                        if not author_el:
+                            continue
+                        # print(author, author_el.text.strip().split()[0])
+                        if not author:
+                            # 作者
+                            author = author_el.text.strip().split()[0]
+                            extract_save(div)
+                        else:
+                            # 只看作者
+                            if author in author_el.text.strip().split()[0]:
+                                extract_save(div)
+
+                next_el = page.find("a", string="下一頁")
+                if next_el:
+                    href = next_el.get("href")
+                    grey = next_el.get("class")
+                    # print(href, grey)
+                    if grey == "gray":
+                        return
+                    a = href.rsplit("/", 1)
+                    if len(a) > 1:
+                        page_download(domain + a[1])
+                    else:
+                        page_download(domain + href)
                 
-                # print(content_el.text)
-            
-        else:
-            print(f"Failed to retrieve data. Status Code: {response.status_code}")
+            else:
+                print(f"Failed to retrieve data. Status Code: {response.status_code}")
 
-    except requests.exceptions.RequestException as e:
-        print(f"Request failed: {e}")
+        except requests.exceptions.RequestException as e:
+            print(f"Request failed: {e}")
+
+    page_download(url)
+    
 
 if __name__ == "__main__":
-    txt()
+    main()
